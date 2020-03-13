0fc0c6305da029bfbe121468ec6b4e13a6e90fe5
amithreddy <amithreddy576r@gmail.com>
2020-03-13 13:21:23 +0530

    Update DEBUG in settings

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECRET_KEY = 'qitv%f)i70#o(vs2i^=4g33@9pq^6plqpp(mytxyx#$elb1^#r'
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['userpostsapp.herokuapp.com']
 
